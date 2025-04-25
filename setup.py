ad0fa3a6ffc35f39c7920943b9211d24d0852f07
Timothe Lyonnet <106395375+TimotheLyonnet@users.noreply.github.com>
2025-04-25 15:54:49 +0100

    Full fix of setup.py for compatible library versions with Google Colab

--- setup.py
+++ setup.py
@@ -31,10 +31,18 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/maudl3116/higherOrderKME",
     packages=setuptools.find_packages(exclude=['examples', 'tests']),
-    install_requires=["cython >= 0.29", "numba == 0.57.0", "torch >= 1.6.0", "numpy==1.20.1", "scipy>=1.6.2", 
-                       "h5py==2.8.0", "matplotlib==3.3.4"], 
+    install_requires=[
+        "cython >= 0.29",
+        "numba == 0.57.0",
+        "llvmlite == 0.40.1",
+        "torch >= 1.6.0",
+        "numpy >= 1.21,<1.25",
+        "scipy >= 1.6.2",
+        "h5py == 2.8.0",
+        "matplotlib == 3.3.4"
+    ],
     python_requires='~=3.6',
-    ext_modules=cythonize(ext_modules), 
+    ext_modules=cythonize(ext_modules),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
