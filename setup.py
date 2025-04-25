016846efcfc5b43144d4e426fd89885eba7ee726
Timothe Lyonnet <106395375+TimotheLyonnet@users.noreply.github.com>
2025-04-25 15:51:24 +0100

    Update numba requirement in setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/maudl3116/higherOrderKME",
     packages=setuptools.find_packages(exclude=['examples', 'tests']),
-    install_requires=["cython >= 0.29", "numba >= 0.50", "torch >= 1.6.0", "numpy==1.20.1", "scipy>=1.6.2", 
+    install_requires=["cython >= 0.29", "numba == 0.57.0", "torch >= 1.6.0", "numpy==1.20.1", "scipy>=1.6.2", 
                        "h5py==2.8.0", "matplotlib==3.3.4"], 
     python_requires='~=3.6',
     ext_modules=cythonize(ext_modules), 
