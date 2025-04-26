2fadf30c4ecbaed55cc3f6bd472058cc17e09cf0
Timothe Lyonnet <106395375+TimotheLyonnet@users.noreply.github.com>
2025-04-26 09:40:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=['examples', 'tests']),
     install_requires=[
         "cython >= 0.29",
-        "numba == 0.56.4",
+        "numba == 0.57.0",
         "llvmlite == 0.40.1",
         "torch >= 1.6.0",
         "numpy >= 1.21,<1.25",
